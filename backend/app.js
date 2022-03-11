53dc72a734c1f27a114871b3eaa7157f3f2171f3
Sujal Duwa <sujalduwa@gmail.com>
2022-03-11 14:14:01 +0545

    Add backend boilerplate

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+/**
+ *
+ * Allows header so that we can pass token for authorization and send request from the client.
+ * @param {Object} req - The request sent by the user.
+ * @param {Object} res - The response sent to the user.
+ * @param {Object} next - The next middleware to be executed.
+ */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// To use JSON data in the request body.
+app.use(bodyParser.json());
+
+/**
+ * Connects to Mongoose cloud database and starts the server.
+ */
+mongoose
+  .connect(process.env.DB_CREDENTIAL)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log('Server ready at ' + port);
+    });
+  })
+  .catch((err) => console.log(err));
